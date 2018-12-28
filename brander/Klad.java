eb6f46ecf312338b8626ee33a36e1f31d2b141d7
ervanden <erik.vanden.meersch@telenet.be>
2018-12-28 19:50:39 +0100

    brander logger

---
+++ brander/Klad.java
@@ -0,0 +1,17 @@
+package brander;
+
+
+import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+public class Klad {
+
+    public static void main(String[] args) {
+        System.out.println("KLAD");
+
+    }
+}
+
