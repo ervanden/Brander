f586d24b3f26134f06678e73273f413d55d49936
ervanden <erik.vanden.meersch@telenet.be>
2018-12-28 20:03:08 +0100

    brander logger

--- brander/Klad.java
+++ brander/Klad.java
@@ -1,17 +1,47 @@
 package brander;
 
 
-import java.io.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
+import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+
 
 
 public class Klad {
 
     public static void main(String[] args) {
         System.out.println("KLAD");
+        GpioController gpio = GpioFactory.getInstance();
 
+        GpioPinDigitalOutput gpio3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "heating", PinState.LOW);
+
+        GpioPinDigitalInput gpio2 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02,
+                "branderSensor", PinPullResistance.PULL_DOWN);
+
+        gpio2.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                System.out.println(
+                        "--> GPIO PIN " + gpio2
+                                + " STATE CHANGE: " + event.getPin()
+                                + " = " + event.getState()
+                );
+            }
+        });
+
+        System.out.println(" ... Listening on " + gpio2.toString());
+        final int SLEEPSEC = 2;
+        try {
+            while (true) {
+                gpio3.setState(true);
+                Thread.sleep(SLEEPSEC * 1000);
+                gpio3.setState(false);
+                Thread.sleep(SLEEPSEC * 1000);
+            }
+        } catch (InterruptedException ie) {
+            System.out.println("Who interrupted the monitor thread???");
+        }
     }
 }
+
 
