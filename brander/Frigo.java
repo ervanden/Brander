89c9ca2b0d5e8fa0b75d1017f09bc4722494e550
ervanden <erik.vanden.meersch@telenet.be>
2019-01-24 18:50:11 +0100

    Frigo

--- brander/Frigo.java
+++ brander/Frigo.java
@@ -1,56 +1,38 @@
 package brander;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Date;
 
 public class Frigo {
 
 
     public static void main(String[] args) throws InterruptedException {
-        try {
-
-            // -- Linux --
-
-            // Run a shell command
-            // Process process = Runtime.getRuntime().exec("ls /home/mkyong/");
-
-            // Run a shell script
-            // Process process = Runtime.getRuntime().exec("path/to/hello.sh");
-
-            // -- Windows --
-
-            // Run a command
-            //Process process = Runtime.getRuntime().exec("cmd /c dir C:\\Users\\mkyong");
-
-            //Run a bat file
-            Process process = Runtime.getRuntime().exec("python /home/pi/dht22temp.py");
-
-            StringBuilder output = new StringBuilder();
-
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(process.getInputStream()));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                output.append(line + "\n");
+        while (true) {
+            try {
+                Process process = Runtime.getRuntime().exec("python /home/pi/dht22temp.py");
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(process.getInputStream()));
+                String line;
+                Integer temperature = null;
+                while ((line = reader.readLine()) != null) {
+                    try {
+                        temperature = Integer.parseInt(line);
+                    } catch (NumberFormatException e) {
+                    }
+                }
+                int exitVal = process.waitFor();
+                if (exitVal == 0) {
+                    if (temperature == null)
+                        System.out.println("null");
+                    else
+                        System.out.println(temperature);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-
-            int exitVal = process.waitFor();
-            if (exitVal == 0) {
-                System.out.println("Success!");
-                System.out.println(output);
-                System.exit(0);
-            } else {
-                //abnormal...
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
 
     }
